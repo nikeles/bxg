b70ce5bc70b3d31ac69386b9477d464a620d8e72
chenliang <1753213342@qq.com>
2017-09-18 18:26:08 +0800

    完成登录功能优化（显示用户头像，用户名），完成退出功能

--- public/js/common.js
+++ public/js/common.js
@@ -1,8 +1,48 @@
 
-	NProgress.start();
+NProgress.start();
 
-	NProgress.done();
+NProgress.done();
 
-	$('.navs ul').prev('a').on('click', function () {
-		$(this).next().slideToggle();
-	});
+$('.navs ul').prev('a').on('click', function () {
+	$(this).next().slideToggle();
+});
+/* 实现退出功能 */
+$('#logout').click(function () {
+	$.ajax({
+		type:'post',
+		url:'/api/logout',
+		dataType:'json',
+		success:function (data) {
+			if(data.code == 200){
+				location.href = '/views/main/login.html';
+			}
+		}
+	})
+});
+/* 判断用户有没有登录 */
+var flag = $.cookie('PHPSESSID');
+if(!flag){
+	location.href = '/views/main/login.html';
+};
+/* 同步头像和用户名 */
+var loginInfo = $.cookie('loginInfo');
+loginInfo = loginInfo && JSON.parse(loginInfo);
+$('.aside .profile img').attr('src',loginInfo.tc_avatar);
+$('.aside .profile h4').html(loginInfo.tc_name);
+console.log($('.aside .profile img')[0].src);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
