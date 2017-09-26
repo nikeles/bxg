b57bde8aaeedf5cde17e641951cdae5a38e6f674
chenliang <1753213342@qq.com>
2017-09-26 10:22:44 +0800

    完成编辑课时的二级联动效果

--- public/js/course_basic.js
+++ public/js/course_basic.js
@@ -1,30 +1,54 @@
-define(['jquery','template','util','form'],function ($,template,util){
+define(['jquery', 'template','ckeditor', 'util', 'form'], function ($, template, CHEDITOR, util) {
     // 实现选选中效果
     util.setAsideActive('/course/course_add');
     // 获取课时id
-    var cs_id=util.qs('cs_id');
+    var cs_id = util.qs('cs_id');   
     // 获取操作标志位
     var flag = util.qs('flag');
     $.ajax({
-        type:'get',
-        url:'/api/course/basic',
-        data:{"cs_id":cs_id},
-        dataType:'json',
-        success:function (data){
-            if(data.code==200){
-                console.log(data);
+        type: 'get',
+        url: '/api/course/basic',
+        data: { "cs_id": cs_id },
+        dataType: 'json',
+        success: function (data) {
+            if (data.code == 200) {
                 // 处理一下标题
-                if(flag){
+                if (flag) {
                     // 编辑操作
-                    data.result.operate='编辑操作';
-                }else{
+                    data.result.operate = '编辑操作';
+                } else {
                     // 添加操作
-                    data.result.operate='添加操作';
+                    data.result.operate = '添加操作';
                 };
-                var html = template('basicTpl',data.result);
+                var html = template('basicTpl', data.result);
                 $("#basicInfo").html(html);
+                // 实现二级联动
+                $("#firstSelect").on('input', function () {
+                    var pid = $(this).val();
+                    $.ajax({
+                        type: 'get',
+                        url: '/api/category/child',
+                        data: { 'cg_id': pid },
+                        dataType: 'json',
+                        success: function (data) {
+                            if (data.code == 200) {
+                                var tpl = '{{each list}}<option value="{{$value.cg_id}}">{{$value.cg_name}}</option>{{/each}}';
+                                var html = template.render(tpl, { list: data.result });
+                                $("#lastSelect").html(html);
+                            }
+                        }
+                    })
+
+                });
+                // 处理富文本
+                CKEDITOR.replace('editor', {
+                    'toolbarGroups': [
+                        {name: 'clipboard', groups: ['clipboard', 'undo']},
+                        {name: 'editing', groups: ['find', 'selection', 'spellchecker', 'editing']}
+                    ]
+                });
             }
-            
+
         }
     })
 })
